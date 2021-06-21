1def9e4752936f4b4a4004e2b8817d6e738e3f1f
Nachi <npatil0821@gmail.com>
2021-06-21 17:32:08 -0400

    Added items for the menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ItemComponent } from './components/item/item.component';
+import { QtyComponent } from './components/qty/qty.component';
+import { LineComponent } from './components/line/line.component';
+import { DonateComponent } from './components/donate/donate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ItemComponent,
+    QtyComponent,
+    LineComponent,
+    DonateComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/qty/qty.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qty',
+  templateUrl: './qty.component.html',
+  styleUrls: ['./qty.component.css']
+})
+export class QtyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
